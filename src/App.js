7273410cb4de45511755f1ca46c01d1149bf69ed
Youssef El MEKKAOUI <92093372+OGSett@users.noreply.github.com>
2024-10-27 17:54:40 +0100

    edit rountes dep2

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Destinations from './pages/DestinationPage';
 import CrewPage from './pages/CrewPage';
 import TechPage from './pages/Technology';
-function App() {
 
-  const [burgerDiv, setBurgerDiv] = useState(false)
+function App() {
+  const [burgerDiv, setBurgerDiv] = useState(false);
 
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.NODE_ENV === 'production' ? '/spx' : ''}>
         <Routes>
-          <Route path='/' element={<Home setBurgerDiv={setBurgerDiv} burgerDiv={burgerDiv}/>}/>
-          <Route path='/destination' element={<Destinations setBurgerDiv={setBurgerDiv} burgerDiv={burgerDiv}/>}/>
-          <Route path='/crew' element={<CrewPage setBurgerDiv={setBurgerDiv} burgerDiv={burgerDiv}/>}/>
-          <Route path='/tech' element={<TechPage setBurgerDiv={setBurgerDiv} burgerDiv={burgerDiv}/>}/>
+          <Route path='/' element={<Home setBurgerDiv={setBurgerDiv} burgerDiv={burgerDiv} />} />
+          <Route path='/destination' element={<Destinations setBurgerDiv={setBurgerDiv} burgerDiv={burgerDiv} />} />
+          <Route path='/crew' element={<CrewPage setBurgerDiv={setBurgerDiv} burgerDiv={burgerDiv} />} />
+          <Route path='/tech' element={<TechPage setBurgerDiv={setBurgerDiv} burgerDiv={burgerDiv} />} />
         </Routes>
       </Router>
     </div>
